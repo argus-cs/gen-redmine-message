900df35c79726b8fdf50364371c727c8f3e29d01
Augusto Santos <augusto_santos55@hotmail.com>
2025-10-31 16:13:32 -0300

    Refactor provider imports and adjust config publishing tag

--- src/RedmineMessageServiceProvider.php
+++ src/RedmineMessageServiceProvider.php
@@ -2,29 +2,22 @@
 
 namespace ArgusCS\RedmineMessage;
 
-use ArgusCS\RedmineMessage\Commands\GenMessage;
 use Illuminate\Support\ServiceProvider;
-use Eqnote\RedmineMessage\Services\RedmineClient;
-use Eqnote\RedmineMessage\Commands\SendRedmineMessage;
+use ArgusCS\RedmineMessage\Commands\GenMessage;
+use ArgusCS\RedmineMessage\Commands\SendRedmineMessage;
 
 class RedmineMessageServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/messages.php', 'messages');
-
-//        $this->app->singleton('messages', function ($app) {
-//            return new RedmineClient();
-//        });
-
-//        $this->app->alias('messages', RedmineClient::class);
     }
 
     public function boot(): void
     {
         $this->publishes([
             __DIR__ . '/../config/messages.php' => config_path('messages.php'),
-        ], 'messages-config');
+        ], 'messages');
 
         if ($this->app->runningInConsole()) {
             $this->commands([
