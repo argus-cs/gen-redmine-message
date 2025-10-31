7f17f9e1c2aab251f4ca1c8ab780da6f6d51af51
Augusto Santos <augusto_santos55@hotmail.com>
2025-10-31 16:13:53 -0300

    Refactor tests to improve coverage and decouple dependencies

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ namespace ArgusCS\RedmineMessage\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use ArgusCS\RedmineMessage\RedmineMessageServiceProvider;
-use ArgusCS\RedmineMessage\Facades\RedmineMessage;
 
 class TestCase extends OrchestraTestCase
 {
@@ -13,21 +12,5 @@ class TestCase extends OrchestraTestCase
         return [
             RedmineMessageServiceProvider::class,
         ];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'RedmineMessage' => RedmineMessage::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('redmine-message.enabled', true);
-        $app['config']->set('redmine-message.endpoint', 'https://redmine.example.com');
-        $app['config']->set('redmine-message.api_token', 'dummy-token');
-        $app['config']->set('redmine-message.default_project', 1);
-        $app['config']->set('redmine-message.timeout', 5);
     }
 }
